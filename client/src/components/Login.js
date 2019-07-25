c6ca51aeec37b4836372187a43313a3882b36d76
Ivan Harasta <harasta.ivan@gmail.com>
2019-07-25 12:46:38 +0200

    Move tcup-client into client folder

---
+++ client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+import SignupForm from './SignupForm';
+
+export default class Login extends Component {
+	render() {
+		return (
+			<Fragment>
+				<SignupForm />
+			</Fragment>
+		);
+	}
+}
