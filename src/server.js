d5836e1eaad461e5afb4f246168d439d3f232567
Ivan Harasta <harasta.ivan@gmail.com>
2019-07-25 13:42:23 +0200

    Setup server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+app.use(express.json());
+
+const db = process.env.MONGO_URI;
+
+mongoose
+	.connect(db, { useNewUrlParser: true, useCreateIndex: true })
+	.then(() => console.log('MongoDB connected'))
+	.catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
