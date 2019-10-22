5ba288f852a1b62c89cad1051efa2ccb6a204e9c
Ivan Harasta <harasta.ivan@gmail.com>
2019-10-23 01:26:46 +0200

    Create tests for all public endpoints

---
+++ test/gliderType.test.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import GliderType from '../src/models/GliderType';
+
+//Require the dev-dependencies
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../src/server';
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+//Our parent block
+describe('GliderType', () => {
+    beforeEach(done => {
+        //Before each test we empty the database
+        GliderType.deleteMany({}, err => {
+            done();
+        });
+    });
+    /*
+     * Test the GET route
+     */
+    describe('GET /api/glidertypes', () => {
+        it('it should get all the glider types', done => {
+            chai.request(server)
+                .get('/api/glidertypes')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(0);
+                    done();
+                });
+        });
+    });
+});
---
+++ test/region.test.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import Region from '../src/models/Region';
+
+//Require the dev-dependencies
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../src/server';
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+//Our parent block
+describe('Region', () => {
+    beforeEach(done => {
+        //Before each test we empty the database
+        Region.deleteMany({}, err => {
+            done();
+        });
+    });
+    /*
+     * Test the GET route
+     */
+    describe('GET /api/regions', () => {
+        it('it should get all the regions', done => {
+            chai.request(server)
+                .get('/api/regions')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(0);
+                    done();
+                });
+        });
+    });
+});
