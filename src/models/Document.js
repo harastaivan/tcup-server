c13fca76f1c541455eb282be59f7fb1093d58311
Ivan Harasta <harasta.ivan@gmail.com>
2019-12-25 13:28:14 +0100

    Create Document model

---
+++ src/models/Document.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+import timestamp from 'mongoose-timestamp';
+
+const Schema = mongoose.Schema;
+
+// Create Schema
+const DocumentSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    path: {
+        type: String,
+        required: true
+    },
+    extension: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: Number,
+        required: true
+    }
+});
+
+DocumentSchema.plugin(timestamp);
+
+const Document = mongoose.model('document', DocumentSchema);
+
+export default Document;
