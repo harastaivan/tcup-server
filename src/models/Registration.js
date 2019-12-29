cd5e7a5c89de8231aaf193b77f64b328235258b7
Ivan Harasta <harasta.ivan@gmail.com>
2019-12-29 16:45:02 +0100

    Implement update registration route

--- src/models/Registration.js
+++ src/models/Registration.js
@@ -5,78 +5,78 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const RegistrationSchema = new Schema({
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'user',
-		required: true
-	},
-	isOnStartingList: {
-		type: Boolean,
-		required: true,
-		default: false
-	},
-	birthDate: {
-		type: String,
-		required: false
-	},
-	phone: {
-		type: String,
-		required: true
-	},
-	aeroclub: {
-		type: String,
-		required: true
-	},
-	region: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'region',
-		required: true
-	},
-	glider: {
-		gliderType: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'gliderType',
-			required: true
-		},
-		registrationNumber: {
-			type: String,
-			required: true
-		},
-		startNumber: {
-			type: String,
-			required: true
-		}
-	},
-	competitionClass: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'competitionClass',
-		required: true
-	},
-	logger: {
-		type: String,
-		required: true
-	},
-	accomodation: {
-		accomodationType: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'accomodationType',
-			required: true
-		},
-		quantity: {
-			type: Number,
-			required: false,
-			default: 0
-		}
-	},
-	meals: {
-		type: Number,
-		required: false,
-		default: 0
-	},
-	note: {
-		type: String,
-		required: false
-	}
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    paid: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    birthDate: {
+        type: String,
+        required: false
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    aeroclub: {
+        type: String,
+        required: true
+    },
+    region: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'region',
+        required: true
+    },
+    glider: {
+        gliderType: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'gliderType',
+            required: true
+        },
+        registrationNumber: {
+            type: String,
+            required: true
+        },
+        startNumber: {
+            type: String,
+            required: true
+        }
+    },
+    competitionClass: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'competitionClass',
+        required: true
+    },
+    logger: {
+        type: String,
+        required: true
+    },
+    accomodation: {
+        accomodationType: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'accomodationType',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: false,
+            default: 0
+        }
+    },
+    meals: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    note: {
+        type: String,
+        required: false
+    }
 });
 
 RegistrationSchema.plugin(timestamp);
