3ca8d9c862d77eee4353c11da5f9853853c3c2ae
Ivan Harasta <harasta.ivan@gmail.com>
2019-12-30 23:19:55 +0100

    Parse json into csv and send file as response

--- src/routes/api/startingList.js
+++ src/routes/api/startingList.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import { parse } from 'json2csv';
 
 import Registration from '../../models/Registration';
 import CompetitionClass from '../../models/CompetitionClass';
 import admin from '../../middleware/admin';
 
 const router = express.Router();
+
+const formatDate = (date = new Date()) => {
+    return date.toISOString().slice(0, 10);
+};
 
 // @route   GET api/starting-list
 // @desc    Get starting list of registered pilots
@@ -48,25 +53,61 @@ router.get('/', async (req, res) => {
 // @desc    Get csv export file of all registered pilots
 // @access  Admin
 router.get('/export', admin, async (req, res) => {
-    const registrations = await Registration.find({})
-        .populate('user', '-password')
-        .populate(['glider.gliderType']);
+    try {
+        const registrations = await Registration.find({})
+            .populate('user', '-password')
+            .populate(['glider.gliderType', 'region', 'competitionClass', 'accomodation.accomodationType']);
 
-    const data = registrations.map((registration) => {
-        return {
-            _id: registration._id,
-            fullName: `${registration.user.name} ${registration.user.surname}`,
-            birthDate: registration.birthDate,
-            aeroclub: registration.aeroclub,
-            startNumber: registration.glider.startNumber,
-            gliderType: `${registration.glider.gliderType.name} (${registration.glider.gliderType.index})`,
-            registrationNumber: registration.glider.registrationNumber,
-            paid: registration.paid,
-            competitionClass: registration.competitionClass
-        };
-    });
+        const docs = registrations.map((registration) => {
+            return {
+                name: registration.user.name,
+                surname: registration.user.surname,
+                birthDate: registration.birthDate,
+                phone: registration.phone,
+                email: registration.user.email,
+                aeroclub: registration.aeroclub,
+                region: registration.region.name,
+                gliderType: registration.glider.gliderType.name,
+                gliderIndex: registration.glider.gliderType.index,
+                registrationNumber: registration.glider.registrationNumber,
+                startNumber: registration.glider.startNumber,
+                competitionClass: registration.competitionClass.name,
+                accomodationType: registration.accomodation.accomodationType.name,
+                accomodationQuantity: registration.accomodation.quantity,
+                meals: registration.meals,
+                note: registration.note,
+                paid: registration.paid ? 'zaplaceno' : 'nezaplaceno'
+            };
+        });
 
-    res.json(data);
+        const fields = [
+            'name',
+            'surname',
+            'birthDate',
+            'phone',
+            'email',
+            'aeroclub',
+            'region',
+            'gliderType',
+            'gliderIndex',
+            'registrationNumber',
+            'startNumber',
+            'competitionClass',
+            'accomodationType',
+            'accomodationQuantity',
+            'meals',
+            'note',
+            'paid'
+        ];
+
+        const opts = { fields };
+        const csv = parse(docs, opts);
+
+        res.attachment(`export-${formatDate()}.csv`);
+        res.status(200).send(csv);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 });
 
 export default router;
