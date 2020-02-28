e9c454ac4a21cf626debcb67ba9a10d1dbdd6ca3
Ivan Harasta <harasta.ivan@gmail.com>
2020-02-28 19:57:48 +0100

    Add autoreconnect and event handlers

--- src/server.js
+++ src/server.js
@@ -26,12 +26,49 @@ app.use(cors());
 
 const db = config.MONGO_URI;
 
-mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then(() => {
-        console.log(`MongoDB connected`);
-    })
-    .catch((err) => console.error(err));
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000
+});
+
+mongoose.connection.on('connecting', () => {
+    console.log('MongoDB connecting');
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected');
+});
+
+mongoose.connection.on('open', () => {
+    console.log('MongoDB connection is open');
+});
+
+mongoose.connection.on('disconnecting', () => {
+    console.error('MongoDB disconnecting');
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.error('MongoDB disconnected');
+});
+
+mongoose.connection.on('close', () => {
+    console.error('MongoDB connection closed');
+});
+
+mongoose.connection.on('reconnected', () => {
+    console.log('MongoDB reconnected');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB error', err);
+});
+
+mongoose.connection.on('fullsetup', () => {
+    console.log('MongoDB fullsetup');
+});
 
 app.use('/', version);
 
