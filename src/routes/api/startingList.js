a7e85213690e47a1cd5e777a3a172b48c7ae235c
Ivan Harasta <harasta.ivan@gmail.com>
2020-07-10 16:11:21 +0200

    Implement see you exports

--- src/routes/api/startingList.js
+++ src/routes/api/startingList.js
@@ -11,6 +11,17 @@ const formatDate = (date = new Date()) => {
     return date.toISOString().slice(0, 10);
 };
 
+const getNationalityByRegion = (region) => {
+    switch (region) {
+        case 'Rakousko':
+            return 'AT';
+        case 'Německo':
+            return 'DE';
+        default:
+            return 'CZ';
+    }
+};
+
 // @route   GET api/starting-list
 // @desc    Get starting list of registered pilots
 // @access  Public
@@ -104,6 +115,86 @@ router.get('/export', admin, async (req, res) => {
         const csv = parse(docs, opts);
 
         res.attachment(`export-${formatDate()}.csv`);
+        res.status(200).send(csv);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+// @route   GET api/starting-list/export/seeyou/:compClass
+// @desc    Get csv export file of all registered pilots
+// @access  Admin
+router.get('/export/seeyou/:compClass', admin, async (req, res) => {
+    try {
+        const { compClass } = req.params;
+
+        const competitionClass = await CompetitionClass.findById(compClass);
+
+        if (!competitionClass) {
+            return res.status(400).json({ msg: 'Invalid competition class' });
+        }
+
+        const registrations = await Registration.find({ competitionClass })
+            .populate('user', '-password')
+            .populate(['glider.gliderType', 'region', 'competitionClass', 'accomodation.accomodationType']);
+
+        const docs = registrations.map((registration) => {
+            return {
+                first_name: registration.user.name,
+                last_name: registration.user.surname,
+                birth_date: registration.birthDate,
+                nationality: getNationalityByRegion(registration.region.name),
+                civl_id: 0,
+                igc_id: 0, // TODO: important
+                email: registration.user.email,
+                phone: registration.phone,
+                club: registration.aeroclub,
+                team: '',
+                sponsors: '',
+                aircraft_model: registration.glider.gliderType.name,
+                contestant_number: registration.glider.startNumber,
+                aircraft_registration: registration.glider.registrationNumber,
+                handicap: registration.glider.gliderType.index,
+                pure_glider: 'False', // TODO
+                // TODO: competitionClass: registration.competitionClass.name,
+                flight_recorders: registration.logger,
+                tag: '',
+                not_competing: 'False',
+                status: '',
+                live_track_id: '',
+                id_contestant: -1
+            };
+        });
+
+        const fields = [
+            'first_name',
+            'last_name',
+            'birth_date',
+            'nationality',
+            'civl_id',
+            'igc_id',
+            'email',
+            'phone',
+            'club',
+            'team',
+            'sponsors',
+            'aircraft_model',
+            'contestant_number',
+            'aircraft_registration',
+            'handicap',
+            'pure_glider',
+            'flight_recorders',
+            'tag',
+            'not_competing',
+            'status',
+            'live_track_id',
+            'id_contestant'
+        ];
+
+        const opts = { fields };
+        const csv = parse(docs, opts);
+
+        res.attachment(`export-${competitionClass.name}-${formatDate()}.csv`);
         res.status(200).send(csv);
     } catch (e) {
         res.status(500).json(e);
