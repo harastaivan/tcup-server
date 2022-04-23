da7933eefb6b4303793e2359dc2685352f41d8c6
Ivan Harasta <ivan.harasta@ackee.cz>
2022-04-24 00:19:39 +0200

    🔥 Remove unused scripts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
-import { config } from 'dotenv';
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
